a557a0a564191d88a9e61360b0033e53ec838a83
Philip Krause <philipkrause7@gmail.com>
2019-08-09 22:14:32 -0400

    added delete

--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -3,24 +3,52 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'user',
   template: `
-    <h1>Hello {{ name }}</h1>
+    <h1>{{ name }}</h1>
     <p><strong>Email</strong> {{ email }}</p>
     <p>
       <strong>Address:</strong> {{ address.street }} {{ address.city }},
       {{ address.state }}
     </p>
 
-    <button (click)="toggleHobbies()">Show Hobbies</button>
+    <button (click)="toggleHobbies()">
+      {{ showHobbies ? 'Hide Hobbies' : 'Show Hobbies' }}
+    </button>
 
-    <div *ngIf='showHobbies'>
+    <div *ngIf="showHobbies">
       <h3>Hobbies</h3>
       <ul>
-      <li *ngFor='let hobby of hobbies'>
-        {{hobby }}
-      </li>
+        <li *ngFor="let hobby of hobbies">
+          {{ hobby }}<button (click)="deleteHobby(i)">X</button>
+        </li>
       </ul>
+      <form (submit)="addHobby(hobby.value)">
+        <label>Add Hobby: </label><br />
+        <input type="text" #hobby /><br />
+      </form>
     </div>
+    <hr />
 
+    <h3>Edit User</h3>
+    <form>
+      <label>Name: </label><br />
+      <input type="text" name="name" [(ngModel)]="name" /><br />
+      <label>Email: </label><br />
+      <input type="text" name="email" [(ngModel)]="email" /><br />
+      <label>Street: </label><br />
+      <input
+        type="text"
+        name="address.street"
+        [(ngModel)]="address.street"
+      /><br />
+      <label>City: </label><br />
+      <input type="text" name="address.city" [(ngModel)]="address.city" /><br />
+      <label>State: </label><br />
+      <input
+        type="text"
+        name="address.state"
+        [(ngModel)]="address.state"
+      /><br />
+    </form>
   `
 })
 export class UserComponent {
@@ -32,7 +60,7 @@ export class UserComponent {
 
   constructor() {
     console.log('constructor ran')
-    this.name = 'John'
+    this.name = 'John Doe'
     this.email = 'john@gmail.com'
     this.address = {
       street: '12 main st',
@@ -42,8 +70,20 @@ export class UserComponent {
     this.hobbies = ['Music', 'Movie', 'Sports']
     this.showHobbies = false
   }
-  toggleHobbies(){
+  toggleHobbies() {
     console.log('button pushed')
+    if (this.showHobbies == true) {
+      this.showHobbies = false
+    } else if (this.showHobbies == false) {
+      this.showHobbies = true
+    }
+  }
+
+  addHobby(hobby: string) {
+    this.hobbies.push(hobby)
+  }
+  deleteHobby(i: number) {
+    this.hobbies.splice(i, 1)
   }
 }
 
